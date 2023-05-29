31f88351ca9464c053f5598410fbc65fcc47d6e5
shanzr <12105513@qq.com>
2023-05-29 19:02:07 +0800

    Initial commit

---
+++ src/settings.4b600.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/scene1.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"00502"}};
